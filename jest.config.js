fb9b268329d41209a645da6049b303562346cfd4
Alexandr <alexandr.florinskiy@vgs.io>
2023-01-19 16:24:24 +0200

    update react-scripts to v5

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
+// module.exports = () => {
+//   return {
+//     preset: "ts-jest",
+//     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+//     setupFilesAfterEnv: ['./jest.setup.js'],
+//   };
+// };
+
 module.exports = {
+  verbose: true,
   preset: "ts-jest",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+  // testMatch: ["(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$"],
+  testMatch: ["**/tests/index.test.tsx"],
   setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: "jsdom",
 };
