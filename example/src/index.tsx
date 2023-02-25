21180d5d7a0e06fc769980bf74a76242c77371dd
Huy Nguyen <huyb.1991@gmail.com>
2023-02-25 09:16:01 +0700

    Fix double quote syntax on example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'
-import { VGSCollectProvider } from "collect-js-react";
+import { VGSCollectProvider } from 'collect-js-react';
 
 ReactDOM.render(
   <VGSCollectProvider>
