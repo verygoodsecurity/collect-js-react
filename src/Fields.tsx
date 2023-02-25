faca8bbac9aff190682c6fd8a33147dab6d88c3b
Huy Nguyen <huyb.1991@gmail.com>
2023-02-25 09:18:05 +0700

    Fix import syntax

--- src/Fields.tsx
+++ src/Fields.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { getFormInstance } from './state';
 import { DEFAULT_CONFIG, FIELD_EVENTS } from './constants';
 
