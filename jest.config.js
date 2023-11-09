2b0229adcc0bb65bde5cf40edf8c99d99dc66b22
Dima Marynych <dima.marynych@verygood.systems>
2023-11-08 16:08:09 -0800

    Added support for react 17

--- jest.config.js
+++ jest.config.js
@@ -5,3 +5,13 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: "jsdom",
 };
+
+if (process.env.REACT_VERSION === '17') {
+  console.log('Testing with React 17');
+
+  module.exports.cacheDirectory = '.cache/jest-cache-react-17';
+  module.exports.moduleNameMapper = {
+    ...module.exports.moduleNameMapper,
+    '^react((\\/.*)?)$': 'react-17$1'
+  };
+}
