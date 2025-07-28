0dccce6c0af955c94913735cc86779e3f5cce1d7
flor <alexandr.florinskiy@vgs.io>
2025-07-29 01:34:53 +0300

    add CMP integration

---
+++ example/src/features/Cmp.tsx
@@ -0,0 +1,132 @@
+import {
+  VGSCollectForm,
+  VGSCollectFormState,
+  VGSCollectHttpStatusCode,
+  VGSCollectVaultEnvironment,
+  useVGSCollectResponse,
+  useVGSCollectState,
+  useVGSCollectFormInstance,
+  IVGSCollectForm
+} from 'collect-js-react';
+import React, { useEffect, useState } from 'react';
+
+import { loadVGSCollect } from '@vgs/collect-js';
+
+const { TextField, CardNumberField, CardExpirationDateField, CardSecurityCodeField } = VGSCollectForm;
+
+const { REACT_APP_VAULT_ID, REACT_APP_ENVIRONMENT, REACT_APP_COLLECT_VERSION } = process.env;
+
+const Cmp = (e: any) => {
+  const [isVGSCollectScriptLoaded, setCollectScriptLoaded] = useState(false);
+  const [formInstance, setFormInstance] = useState<IVGSCollectForm | null>(null);
+  const [isFormSubmitting, setFormSubmitting] = useState(false);
+  const VGSCollectFieldStyles = {
+    '&::placeholder': {
+      color: '#686868'
+    },
+    padding: '.5rem 1rem',
+    boxSizing: 'border-box'
+  };
+
+  const [state] = useVGSCollectState();
+  const [response] = useVGSCollectResponse();
+  const [form] = useVGSCollectFormInstance();
+
+  useEffect(() => {
+    /**
+     * Track form state
+     */
+  }, [state]);
+
+  useEffect(() => {
+    /**
+     * Track response from the VGS Collect form
+     */
+  }, [response]);
+
+  useEffect(() => {
+    setFormInstance(form);
+  }, [form]);
+
+  useEffect(() => {
+    /**
+     * Loading VGS Collect script from and attaching it to the <head>
+     */
+    loadVGSCollect({
+      vaultId: REACT_APP_VAULT_ID as string,
+      environment: REACT_APP_ENVIRONMENT as VGSCollectVaultEnvironment,
+      version: REACT_APP_COLLECT_VERSION as string
+    }).then(() => {
+      setCollectScriptLoaded(true);
+    });
+  }, []);
+
+  const onErrorCallback = (errors: VGSCollectFormState) => {
+    /**
+     * Receive information about Erorrs (client-side validation, or rejection in async headers function)
+     */
+  };
+
+  const onUpdateCallback = (state: VGSCollectFormState) => {
+    /**
+     * Listen to the VGS Collect form state
+     */
+  };
+
+  const onCardCreateCallback = (status: VGSCollectHttpStatusCode, resp: any) => {
+    console.log('Submit callback')
+  }
+ 
+  return (
+    <>
+      {isVGSCollectScriptLoaded && (
+        <div className='left'>
+          <h2>CMP</h2>
+          {/**
+           * VGS Collect form wrapper element. Abstraction over the VGSCollect.create()
+           * https://www.verygoodsecurity.com/docs/api/collect/#api-vgscollectcreate
+           */}
+          <VGSCollectForm
+            vaultId={REACT_APP_VAULT_ID as string}
+            environment={REACT_APP_ENVIRONMENT as VGSCollectVaultEnvironment}
+            onUpdateCallback={onUpdateCallback}
+            onErrorCallback={onErrorCallback}
+            onCardCreateCallback={onCardCreateCallback}
+            submitParameters={{
+              auth: 'token.access_token', 
+              data: {
+                cardholder: {
+                  name: 'test',
+                  address: {
+                      address1: '123 Main St',
+                      address2: 'Suite 456',
+                      address3: 'Line 3',
+                      address4: 'Line 4',
+                      city: 'LA',
+                      region: 'CA',
+                      postal_code: '12345',
+                      country: 'USA'
+                  }
+                }
+              }
+            }}
+          >
+            {/**
+             * VGS Collect text field component:
+             * https://www.verygoodsecurity.com/docs/api/collect/#api-formfield
+             */}
+            <TextField name='textField' validations={['required']} css={VGSCollectFieldStyles} />
+            <CardNumberField css={VGSCollectFieldStyles} />
+            <CardExpirationDateField css={VGSCollectFieldStyles} />
+            <CardSecurityCodeField css={VGSCollectFieldStyles} />
+            
+            <button type='submit'>Submit</button>
+
+          </VGSCollectForm>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Cmp;
